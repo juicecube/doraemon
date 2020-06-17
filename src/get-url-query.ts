2c8b4aa65cc31d2f1e88f08db153bbc4c976d9d1
urnotzane <urnotzane@163.com>
2020-06-18 01:03:14 +0800

    feat: 获取url参数的方法+test

---
+++ src/get-url-query.ts
@@ -0,0 +1,24 @@
+export const getUrlQuery = (url?:string) => {
+  const result:IObject = {};
+  // 不是string
+  if (typeof url !== 'string') {
+    return result;
+  }
+  const start = url.indexOf('?');
+  // 没有参数
+  if (start === -1 ) {
+    return result;
+  }
+  // 有?号没参数
+  if (start === url.length - 1) {
+    return result;
+  }
+  const queryStr = url.substring(start + 1);
+  queryStr.split('&').forEach((item) => {
+    const equalIndex = item.indexOf('=');
+    if (equalIndex > -1) {
+      result[item.substring(0, equalIndex)] = item.substring(equalIndex + 1);
+    }
+  });
+  return result;
+};
