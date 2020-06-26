6695ed532ca1435f41b29622da0fe7b559bfbeff
urnotzane <zangleran@codemao.cn>
2020-06-26 17:04:24 +0800

    fix: 修改father-build配置

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,21 +1,11 @@
-import typescript from 'rollup-plugin-typescript2';
-
 export default {
-  entry: ['src/index.ts'],
-  overridesByEntry: {
-    'src/index.ts': {
-      file: `./dist/es/index`,
-    },
-  },
+  entry: 'src/index.ts',
   esm: {
     type: 'babel',
     importLibToEs: true,
   },
-  cjs: 'babel',
-  extraRollupPlugins: [
-    typescript({
-      useTsconfigDeclarationDir: true,
-      tsconfig: 'tsconfig.json',
-    }),
-  ],
+  cjs: {
+    type: 'babel',
+    lazy: true,
+  },
 };
