1f4304c06bc5743091c7949cf13e7b2d7f19e285
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-12 12:40:10 +0000

    Deploy juicecube/doraemon to github.com/juicecube/doraemon.git:gh-pages

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { getUrlQuery } from './getUrlQuery';
+import { addQueryToUrl } from './addQueryToUrl';
+import {
+  isNull, isNumber, isArray, isDate, isObject, isString,
+  isFunction, isBoolean, isRegExp, isUndefined,
+} from './typeChecker';
+
+export {
+  isNull,
+  isUndefined,
+  isNumber,
+  isBoolean,
+  isArray,
+  isDate,
+  isRegExp,
+  isObject,
+  isString,
+  isFunction,
+  getUrlQuery,
+  addQueryToUrl,
+};
+
+export const doraemaon = {
+  isNull,
+  isUndefined,
+  isNumber,
+  isBoolean,
+  isArray,
+  isDate,
+  isRegExp,
+  isObject,
+  isString,
+  isFunction,
+  getUrlQuery,
+  addQueryToUrl,
+};
+
+export default doraemaon;
