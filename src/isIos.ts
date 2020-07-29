71feb18ede0f3a4ddc7b13f89da7dad1229b4ae4
urnotzane <urnotzane@163.com>
2020-07-29 16:58:39 +0800

    feat: isMobile|isWechat

--- src/isIos.ts
+++ src/isIos.ts
@@ -1,3 +1,4 @@
+/** 是否在ios环境内 */
 export const isIos = () => {
   const ua = navigator.userAgent;
   return !!ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
--- test/userAgent.test.ts
+++ test/userAgent.test.ts
@@ -1,8 +1,10 @@
+import { isMobile } from './../src/isMobile';
+import { isWechat } from './../src/isWechat';
 import { UserAgentSetter } from './utils/user-agent-setter';
 import { isIos } from '../src/isIos';
 import { userAgentMap } from './constants.ts/user-agent';
 
-describe('isIos', () => {
+describe('isIos|isWechat', () => {
   const userAgentSetter = new UserAgentSetter();
   afterAll(() => {
     userAgentSetter.reset();
@@ -10,25 +12,43 @@ describe('isIos', () => {
   it('In ios wechat', () => {
     userAgentSetter.setUserAgent(userAgentMap.wechatInIos);
     expect(isIos()).toBeTruthy();
+    expect(isWechat()).toBeTruthy();
+    expect(isMobile()).toBeTruthy();
   });
   it('In ios mobile', () => {
     userAgentSetter.setUserAgent(userAgentMap.mobileInIos);
     expect(isIos()).toBeTruthy();
+    expect(isWechat()).toBeFalsy();
+    expect(isMobile()).toBeTruthy();
   });
   it('In Android wechat', () => {
     userAgentSetter.setUserAgent(userAgentMap.wechatInAndroid);
     expect(isIos()).toBeFalsy();
+    expect(isWechat()).toBeTruthy();
+    expect(isMobile()).toBeTruthy();
   });
   it('In Android mobile', () => {
     userAgentSetter.setUserAgent(userAgentMap.mobileInAndroid);
     expect(isIos()).toBeFalsy();
+    expect(isWechat()).toBeFalsy();
+    expect(isMobile()).toBeTruthy();
   });
   it('In Mac wechat', () => {
     userAgentSetter.setUserAgent(userAgentMap.wechatInMac);
     expect(isIos()).toBeFalsy();
+    expect(isWechat()).toBeTruthy();
+    expect(isMobile()).toBeFalsy();
   });
   it('In Mac mobile', () => {
     userAgentSetter.setUserAgent(userAgentMap.chromeInMac);
     expect(isIos()).toBeFalsy();
+    expect(isWechat()).toBeFalsy();
+    expect(isMobile()).toBeFalsy();
+  });
+  it('In window wechat', () => {
+    userAgentSetter.setUserAgent(userAgentMap.wechatInWindows);
+    expect(isIos()).toBeFalsy();
+    expect(isWechat()).toBeTruthy();
+    expect(isMobile()).toBeFalsy();
   });
 });
