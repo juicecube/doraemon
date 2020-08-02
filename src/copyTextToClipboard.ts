1dacc93f1dcb5277679edcca8239c09eae02ac64
urnotzane <urnotzane@163.com>
2020-08-02 15:40:48 +0800

    feat: copyTextToClipboard

---
+++ src/copyTextToClipboard.ts
@@ -0,0 +1,25 @@
+import isIos from './isIos';
+
+/** 复制内容并返回复制是否成功 */
+export const copyTextToClipboard = (value:string):Promise<boolean> => new Promise((resolve) => {
+  try {
+    const oInput = document.createElement('input');
+    oInput.value = value;
+    document.body.appendChild(oInput);
+    if (isIos()) {
+      // 防止低ios系统版本（ios 11）出现弹出小键盘
+      oInput.disabled = true;
+      oInput.setSelectionRange(0, value.length);
+    } else {
+      oInput.select();
+    }
+    document.execCommand('Copy');
+    oInput.style.display = 'none';
+    document.body.removeChild(oInput);
+    resolve(true);
+  } catch (error) {
+    resolve(false);
+  }
+});
+
+export default copyTextToClipboard;
