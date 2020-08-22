eda36209d22c2ba7cd1490e457769640b66f0d74
urnotzane <urnotzane@163.com>
2020-08-22 13:54:54 +0800

    fix: domain

--- test/addQueryToUrl.test.ts
+++ test/addQueryToUrl.test.ts
@@ -1,6 +1,6 @@
 import { addQueryToUrl } from '../src/addQueryToUrl';
+import { domain } from '../src/constants';
 
-const url = 'https://doraemon.urnotzane.tech';
 const query = {
   name: 'doraemon',
   time: 2020,
@@ -15,24 +15,24 @@ const queryUndefined = {
   time: 2020,
   author: undefined,
 };
-const result = 'https://doraemon.urnotzane.tech?name=doraemon&time=2020';
+const result = 'https://doraemon.ithen.cn?name=doraemon&time=2020';
 const parameterStr = '?name=doraemon&time=2020';
 
 describe('addQueryToUrl', () => {
   it('query is undefined', () => {
-    expect(addQueryToUrl(undefined, url)).toEqual(url);
+    expect(addQueryToUrl(undefined, domain)).toEqual(domain);
   });
   it('query is not a real object', () => {
-    expect(addQueryToUrl([1, 2], url)).toEqual(url);
+    expect(addQueryToUrl([1, 2], domain)).toEqual(domain);
   });
   it('query is a empty object', () => {
-    expect(addQueryToUrl({}, url)).toEqual(url);
+    expect(addQueryToUrl({}, domain)).toEqual(domain);
   });
   it('query has a null element', () => {
-    expect(addQueryToUrl(queryNull, url)).toEqual(result);
+    expect(addQueryToUrl(queryNull, domain)).toEqual(result);
   });
   it('query has a undefined element', () => {
-    expect(addQueryToUrl(queryUndefined, url)).toEqual(result);
+    expect(addQueryToUrl(queryUndefined, domain)).toEqual(result);
   });
   it('url is undefined', () => {
     expect(addQueryToUrl(query)).toEqual(parameterStr);
@@ -44,20 +44,20 @@ describe('addQueryToUrl', () => {
     expect(addQueryToUrl(query, '')).toEqual(parameterStr);
   });
   it('has a `?` in url', () => {
-    expect(addQueryToUrl(query, `${url}?`)).toEqual(result);
+    expect(addQueryToUrl(query, `${domain}?`)).toEqual(result);
   });
   it('has a `?` and the last character is `&` in url', () => {
-    const _url = `${url}?author=urnotzane`;
+    const _url = `${domain}?author=urnotzane`;
     expect(addQueryToUrl(query, `${_url}&`)).toEqual(`${_url}&name=doraemon&time=2020`);
   });
   it('a parameter has been added to the end of the url', () => {
-    const _url = `${url}?author=urnotzane`;
+    const _url = `${domain}?author=urnotzane`;
     expect(addQueryToUrl(query, _url)).toEqual(`${_url}&name=doraemon&time=2020`);
   });
   it('query & url are undefined', () => {
     expect(addQueryToUrl(undefined, undefined)).toEqual('');
   });
   it('query & url are reasonable', () => {
-    expect(addQueryToUrl(query, url)).toEqual(result);
+    expect(addQueryToUrl(query, domain)).toEqual(result);
   });
 });
